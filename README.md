ce42256f59b3fba15103a65ce8622283f6fbb417
SkyrixFr <63282721+SkyrixFr@users.noreply.github.com>
2023-04-13 22:18:27 +0200

    13/04/2023 : added a few quizzes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# just-move-it
-a school project for just dance clone
+<h1>just-move-it</h1>
+
+#### a school project for just dance clone
+
+<br/>
+
+---
+## How to setup and enter env
+* python -m venv env
+* .\env\Scripts\activate
+* python -m pip install -r requirements.txt
+
+## Start the program with main.py
+
+---
+## TODO:
+* Find a great idea
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=2,
+                      max_num_hands=1,
                       min_detection_confidence=0.5,
                       min_tracking_confidence=0.5)
 mpDraw = mp.solutions.drawing_utils
@@ -19,13 +19,19 @@ while True:
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
+        #print(results.multi_hand_landmarks)
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                print(lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x *w), int(lm.y*h)
                 #if id ==0:
                 cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+                if lm.x>0.5:
+                    print("gauche")
+                if lm.x<=0.5:
+                    print("droite")
+                
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
